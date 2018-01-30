cd823c9684fe5db2c2005ad34ee2c78881aaffec
Mike Danko <mike@l4m3.com>
2018-01-30 14:33:34 -0500

    change root route

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get 'books/random'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'books#random'
+  root to: 'categories#index'
 end
