677c12110d353d7f0872088a0c0db63ad91f3527
Mike Danko <mike@l4m3.com>
2018-02-15 14:33:33 -0500

    add reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,27 @@
 class ReviewsController < ApplicationController
   def index
     @book = Book.find(params[:book_id])
-    @reviews = Review.where(book_id: params[:book_id]).page(params[:page]).per(5)
+    @reviews = Review.where(book_id: params[:book_id]).order(created_at: :desc).page(params[:page]).per(5)
+  end
+
+  def new
+    @book = Book.find(params[:book_id])
+    @review = @book.reviews.new
+  end
+
+  def create
+    @book = Book.find(params[:book_id])
+    @review = @book.reviews.new(review_params)
+    @review.rating = rand(1..5)
+    @review.reviewer_name = FFaker::Name.name
+    if @review.save
+      redirect_to category_book_url(@book.category, @book), notice: "Thanks #{@review.reviewer_name}! Review Submitted!"
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:review)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
 
   resources :books do
-    resources :reviews, only: [:index]
+    resources :reviews, only: [:index, :new, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'categories#index'
